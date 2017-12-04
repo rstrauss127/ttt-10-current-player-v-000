93b226fcf6da5676f50371018aa4c186de97bce1
Rebecca Strauss <rstrauss127@gmail.com>
2017-12-04 21:01:40 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+def turn_count(board)
+  count = 0
+  turn = 0
+  while count <= 9
+    if (count == 9)
+      return turn
+    if ((board[count] == "X") || board[count] == "O")
+      turn += 1
+      count += 1
+    else
+      count += 1
+  
+    end
+  end
+end
